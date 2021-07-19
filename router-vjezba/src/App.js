17f1e2097a7f4a185ddd88407d68918a0b7b752c
Matija <matija.droid@gmail.com>
2021-07-19 21:03:56 +0200

    vjezba

--- router-vjezba/src/App.js
+++ router-vjezba/src/App.js
@@ -1,8 +1,37 @@
+import {BrowserRouter, Route, Link} from 'react-router-dom'; //BrowserRouter može biti i u index JS ali mora biti unutar {}
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Services from './pages/Services';
+
+
 function App() {
   return (
-    <div>
-      Moja prva aplikacija.
-    </div>  //u reactu mora postojati parent element
+    <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/home">Home</Link> 
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <li>
+                <Link to="/services">Services</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact Us</Link>
+              </li>
+            </ul>
+            {/*za link koristimo to= a ne href kak se koristi u HTML-u*/}
+          </nav>
+
+          <Path path="/" components={Home} />
+          
+        </div> 
+    </BrowserRouter>
+
   );
 }
 

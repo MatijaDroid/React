94bb95a0933f0c3f285175c5081964eba6e88994
Matija <matija.droid@gmail.com>
2021-07-27 18:04:00 +0200

    add

--- Redux-vjezba/src/App.js
+++ Redux-vjezba/src/App.js
@@ -1,8 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    <div>
-      Moja prva aplikacija.
-    </div>  //u reactu mora postojati parent element
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 

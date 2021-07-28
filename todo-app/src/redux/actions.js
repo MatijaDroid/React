d148f3d5914349bbc7d1a2927b51dd235e0309e4
Matija <matija.droid@gmail.com>
2021-07-28 19:28:09 +0200

    todo vjezba

--- todo-app/src/redux/actions.js
+++ todo-app/src/redux/actions.js
@@ -1,1 +1,13 @@
 //ne radi se o react komponentama pa nam netreba Pascal case
+//definiramo action koji koristimo za reducer
+import uniqueString from 'unique-string' // importamo modul koji omogućava stvaranje unique stringova za kreiranje unikatnih id-jeva
+
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const REMOVE_TODO = 'REMOVE_TODO';
+export const REMOVE_ALL_COMPLETED = 'REMOVE_ALL_COMPLETED';
+
+
+export function addTodo(text){ //text šaljemo preko parametra iz user input valuea
+    return {type: ADD_TODO, id: uniqueString(), text}; // pozivamo metodu unique string koja nam generira unikatan id za unos
+}
--- todo-app/src/redux/reducer.js
+++ todo-app/src/redux/reducer.js
@@ -1,7 +1,21 @@
 import { combineReducers } from 'redux';
+import {  // nemoramo ovako pisati ali je preglednije
+    ADD_TODO, 
+    TOGGLE_TODO, 
+    REMOVE_TODO, 
+    REMOVE_ALL_COMPLETED 
+} from './actions';
 
 const todos = (state = [], action) => {
     switch(action.type){
+        case ADD_TODO:
+            return;
+        case TOGGLE_TODO:
+            return;
+        case REMOVE_TODO:
+            return;
+        case REMOVE_ALL_COMPLETED:
+            return;
         default: //nikad ne izostaviti return state;
             return state;
     }
